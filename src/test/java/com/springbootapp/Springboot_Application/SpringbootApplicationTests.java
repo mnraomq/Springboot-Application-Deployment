e7baa5dd5a360b2738b4d378cf510762a7c62305
venkat akurathi <venkatakurathi@MACBOOKPRO-867B.station>
2025-03-22 20:34:37 +0000

    add springboot application to main branch

---
+++ src/test/java/com/springbootapp/Springboot_Application/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapp.Springboot_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
